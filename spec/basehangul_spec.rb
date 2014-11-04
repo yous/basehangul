91759e0c95b9652566ba383c1a4970ca5991de3a
ChaYoung You <yousbe@gmail.com>
2014-11-05 02:40:37 +0900

    Extend the range of BaseHangul
    See
    https://github.com/koreapyj/basehangul/commit/8a5026dd62ee2e4771dfd6831820b568fd4d9e32
    and http://api.dcmys.jp/basehangul/.

--- spec/basehangul_spec.rb
+++ spec/basehangul_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe BaseHangul do
 
   describe '#to_index' do
     it 'converts hangul to index' do
-      range = ('가'.encode(Encoding::EUC_KR)..'빌'.encode(Encoding::EUC_KR))
+      range = ('가'.encode(Encoding::EUC_KR)..'빗'.encode(Encoding::EUC_KR))
               .map { |v| v.encode(Encoding::UTF_8) }
       range.each_with_index do |hangul, expected|
         index = basehangul.send(:to_index, hangul)
@@ -20,7 +20,7 @@ RSpec.describe BaseHangul do
     end
 
     it 'raises ArgumentError for invalid hangul' do
-      range = ('빎'.encode(Encoding::EUC_KR)..'힝'.encode(Encoding::EUC_KR))
+      range = ('빙'.encode(Encoding::EUC_KR)..'힝'.encode(Encoding::EUC_KR))
               .map { |v| v.encode(Encoding::UTF_8) }
       range.reject { |v| v == padding }.each do |hangul|
         expect { basehangul.send(:to_index, hangul) }
@@ -31,19 +31,19 @@ RSpec.describe BaseHangul do
 
   describe '#to_hangul' do
     it 'converts index to hangul' do
-      range = ('가'.encode(Encoding::EUC_KR)..'빌'.encode(Encoding::EUC_KR))
+      range = ('가'.encode(Encoding::EUC_KR)..'빗'.encode(Encoding::EUC_KR))
               .map { |v| v.encode(Encoding::UTF_8) }
-      (0..1023).each do |index|
+      (0..1027).each do |index|
         hangul = basehangul.send(:to_hangul, index)
         expect(range.index(hangul)).to eq(index)
       end
     end
 
     it 'raises IndexError for index out of range' do
-      [-100, -1, 1024, 2000].each do |index|
+      [-100, -1, 1028, 2000].each do |index|
         expect { basehangul.send(:to_hangul, index) }
           .to raise_error(IndexError, "Index #{index} outside of valid " \
-                                      'range: 0..1023')
+                                      'range: 0..1027')
       end
     end
   end
