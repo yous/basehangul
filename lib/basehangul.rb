880e29ca9d1bcf445fbc8ff362f71aeaa4c4cf95
ChaYoung You <yousbe@gmail.com>
2014-11-05 04:12:09 +0900

    Move util methods to Utils module

--- lib/basehangul.rb
+++ lib/basehangul.rb
@@ -1,42 +1,11 @@
 # encoding: utf-8
 
 require 'basehangul/version'
+require 'basehangul/utils'
 
 # Binary encoder using hangul.
 module BaseHangul
   # Character for padding on encoding.
   PADDING = '흐'.freeze
   private_constant :PADDING
-
-  private
-
-  # Convert a hangul character to index.
-  #
-  # hangul - A hangul character encoded with BaseHangul.
-  #
-  # Returns the Integer index of the hangul between 0 to 1027.
-  # Raises ArgumentError if the character is not valid for BaseHangul.
-  def self.to_index(hangul)
-    return -1 if hangul == PADDING
-    offset = hangul.encode(Encoding::EUC_KR).ord - 0xB0A1
-    index = offset / 0x100 * 0x5E + offset % 0x100
-    if index < 0 || index > 1027
-      fail ArgumentError, 'Not a valid BaseHangul string'
-    end
-    index
-  end
-
-  # Convert a index to hangul character.
-  #
-  # index - An Integer to convert.
-  #
-  # Returns the String hangul for given index.
-  # Raises IndexError if the index is out of range 0..1027.
-  def self.to_hangul(index)
-    if index < 0 || index > 1027
-      fail IndexError, "Index #{index} outside of valid range: 0..1027"
-    end
-    (index / 0x5E * 0x100 + index % 0x5E + 0xB0A1).chr(Encoding::EUC_KR)
-      .encode(Encoding::UTF_8)
-  end
 end
--- spec/basehangul/utils_spec.rb
+++ spec/basehangul/utils_spec.rb
@@ -1,21 +1,21 @@
 # encoding: utf-8
 
-RSpec.describe BaseHangul do
-  subject(:basehangul) { described_class }
-  let(:padding) { basehangul.const_get(:PADDING) }
+RSpec.describe BaseHangul::Utils do
+  subject(:utils) { described_class }
+  let(:padding) { BaseHangul.const_get(:PADDING) }
 
-  describe '#to_index' do
+  describe '.to_index' do
     it 'converts hangul to index' do
       range = ('가'.encode(Encoding::EUC_KR)..'빗'.encode(Encoding::EUC_KR))
               .map { |v| v.encode(Encoding::UTF_8) }
       range.each_with_index do |hangul, expected|
-        index = basehangul.send(:to_index, hangul)
+        index = utils.to_index(hangul)
         expect(index).to eq(expected)
       end
     end
 
     it 'converts padding hangul to -1' do
-      index = basehangul.send(:to_index, padding)
+      index = utils.to_index(padding)
       expect(index).to eq(-1)
     end
 
@@ -23,25 +23,25 @@ RSpec.describe BaseHangul do
       range = ('빙'.encode(Encoding::EUC_KR)..'힝'.encode(Encoding::EUC_KR))
               .map { |v| v.encode(Encoding::UTF_8) }
       range.reject { |v| v == padding }.each do |hangul|
-        expect { basehangul.send(:to_index, hangul) }
+        expect { utils.to_index(hangul) }
           .to raise_error(ArgumentError, 'Not a valid BaseHangul string')
       end
     end
   end
 
-  describe '#to_hangul' do
+  describe '.to_hangul' do
     it 'converts index to hangul' do
       range = ('가'.encode(Encoding::EUC_KR)..'빗'.encode(Encoding::EUC_KR))
               .map { |v| v.encode(Encoding::UTF_8) }
       (0..1027).each do |index|
-        hangul = basehangul.send(:to_hangul, index)
+        hangul = utils.to_hangul(index)
         expect(range.index(hangul)).to eq(index)
       end
     end
 
     it 'raises IndexError for index out of range' do
       [-100, -1, 1028, 2000].each do |index|
-        expect { basehangul.send(:to_hangul, index) }
+        expect { utils.to_hangul(index) }
           .to raise_error(IndexError, "Index #{index} outside of valid " \
                                       'range: 0..1027')
       end
